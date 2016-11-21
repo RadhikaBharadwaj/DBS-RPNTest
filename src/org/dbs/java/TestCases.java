a258e8ef950e9746e94cef7ab9bb536e15b96eb3
Admin <Admin@Admin-PC>
2016-11-21 12:06:48 +0530

    First Commit

---
+++ src/org/dbs/java/TestCases.java
@@ -0,0 +1,40 @@
+package org.dbs.java;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class TestCases {
+
+	@Given("^The two numbers are (\\d+) and (\\d+)$")
+	public void The_two_numbers_are_and(int arg1, int arg2) throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@When("^Upon addition$")
+	public void Upon_addition() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Then("^result of (\\d+) must be displayed$")
+	public void result_of_must_be_displayed(int arg1) throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@When("^perform difference of two numbers$")
+	public void perform_difference_of_two_numbers() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@When("^perform multiplication of two numbers$")
+	public void perform_multiplication_of_two_numbers() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+	
+}
---
+++ src/org/dbs/resources/RPN.feature
@@ -0,0 +1,19 @@
+Feature: Acceptance of arithmetic operations
+As a user for performing the RPN calculation i need to perform the sum , difference, multiplication and division.
+
+Scenario: Perform sum of operands
+
+Given The numbers are 2 and 4 
+When Upon addition
+Then result of 6 must be displayed
+
+Scenario: Perform difference of operands
+
+Given Input numbers are 6 and 4 
+When perform difference of two numbers
+Then result of 2 must be displayed
+
+
+
+
+
